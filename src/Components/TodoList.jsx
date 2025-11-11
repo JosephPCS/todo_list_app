a4c15df96493701595227eeeaab6b4a2442d3438
Joseph Pleasant <josephpleasant0@gmail.com>
2025-11-11 13:50:32 -0500

    finished to do list app

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -2,7 +2,42 @@ import React, { useState } from 'react';
 import './TodoList.css';
 
 const TodoList = () => {
-  
+  const [todos,setTodos] = useState([]);
+  const [headingInput, setHeadingInput] = useState("");
+  const [listInputs,setListInputs] = useState({});
+
+  const handleAddTodo = () =>{
+    if(headingInput.trim() !== ""){
+        setTodos([...todos, {heading : headingInput, lists:[]} ]); //this is spread syntax
+        //the spread syntax creates an new object with a heading proeprty that is set to heading input
+        //also initializes an empty array called lists, im assuming this is what is used for the sub to-dos of the corresponding heading
+        setHeadingInput("");
+    }
+
+   
+   
+  };
+
+  const handleAddList = (index) => {
+    if(listInputs[index].trim() !== '' && listInputs[index]){
+        const newTodos = [...todos];
+        newTodos[index].lists.push(listInputs[index]);
+        setTodos(newTodos);
+        setListInputs({...listInputs, [index]: ''});
+
+    }
+};
+
+const handleListInputChange = (index, value) =>{
+    setListInputs({...listInputs, [index]: value});
+};
+
+const handleDeleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index,1);
+    setTodos(newTodos);
+
+};
 
   return (
     <>
@@ -12,14 +47,52 @@ const TodoList = () => {
           <input
             type="text"
             className="heading-input"
+            value={headingInput}
+            onChange={(e) => {setHeadingInput(e.target.value);}}
             placeholder="Enter heading"
             
           />
-          <button className="add-list-button">Add Heading</button>
+          <button className="add-list-button" onClick={handleAddTodo}>Add Heading</button>
         </div>
       </div>
       <div className="todo_main">
+        {todos.map((todo, index) => (
+            <div key={index} className='todo-card'>
+                <div className='heading_todo'>
+                    <h3>{todo.heading}</h3>
+                    <button
+                                className="delete-button-heading"
+                                onClick={() => handleDeleteTodo(index)}
+                            >
+                                Delete Heading
+                            </button>
+                </div>
+
+                <ul>
+                    {todo.lists.map((list, listIndex) => (
+                        <li key={listIndex} className='todo_inside_list'>
+                            <p>{list}</p>
+                        </li>
+                    ))}
+                </ul>
+
+         <div className='add_list'>
+            {/* Input field for adding a new item under a specific heading */}
+            <input
+                type="text"
+                className="list-input"
+                placeholder="Add List"
+                value={listInputs[index] || ''}// Use the value from listInputs array based on the current heading index
+                onChange={(e) => handleListInputChange(index, e.target.value)}
+                />
+            {/* Button to add the list item to the corresponding heading */}
+            <button className="add-list-button" onClick={() => handleAddList(index)}>Add List</button>
+         </div>
+               
+            </div>
+        ))}
         
+       
       </div>
     </>
   );
